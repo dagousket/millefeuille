c2d25af4d8406c4f1cd3fd8cf140ba5597f0cb68
dagousket <swann.flochlay@gmail.com>
2025-05-23 15:01:02 +0200

    :sparkles:  feat: add overlap function def and test

--- millefeuille/module/overlaps.py
+++ millefeuille/module/overlaps.py
@@ -2,9 +2,10 @@ import os
 import sys
 import argparse
 import pyranges as pr, pandas as pd
+from itertools import combinations
 
 
-def load_beds(list_bed: list) -> list:
+def load_beds(list_bed: list, names: list = ["a", "b", "c"]) -> dict:
     """
     Create a list of pyranges intervals from a list of bed files.
 
@@ -16,13 +17,74 @@ def load_beds(list_bed: list) -> list:
     Returns
     -------
     list
-      A list of pyranges intervals.
+      A dict of pyranges intervals.
     """
     if len(list_bed) != 3:
         raise ValueError("list_bed must be of length 3")
     if not all(os.path.isfile(bed) for bed in list_bed):
         raise ValueError("All elements of list_bed must be valid file paths")
 
-    pr_list = [pr.readers.read_bed(bed) for bed in list_bed]
+    pr_dict = dict(zip(names, [pr.readers.read_bed(bed) for bed in list_bed]))
 
-    return pr_list
+    return pr_dict
+
+
+def double_overlap(pr_dict: dict) -> dict:
+    """
+    Calculate the overlaps between the two pyranges intervals.
+    Only returns regions that do not overlap with the third interval.
+
+    Parameters
+    ----------
+    pr_dict : dict
+      A dict of pyranges intervals.
+    output : dict
+      The dictionnary of intervals that overlap with the two pyranges intervals.
+    """
+    bedpair_output = dict()
+    for bedpair in combinations(pr_dict.keys(), 2):
+        pair_overlap = pr_dict[bedpair[0]].intersect(pr_dict[bedpair[1]])
+        last_overlap = [bed for bed in pr_dict.keys() if bed not in bedpair][0]
+        pair_overlap = pair_overlap.overlap(pr_dict[last_overlap], invert=True)
+        bedpair_output["::".join(bedpair)] = pair_overlap
+
+    return bedpair_output
+
+
+def triple_overlap(pr_dict: dict) -> dict:
+    """
+    Calculate the overlaps between the three pyranges intervals.
+
+    Parameters
+    ----------
+    pr_dict : dict
+      A dict of pyranges intervals.
+    output : dict
+      The intervals that overlap with all three intervals.
+    """
+    pr_dict_val = list(pr_dict.values())
+    triple_overlap = pr_dict_val[0].intersect(pr_dict_val[1]).intersect(pr_dict_val[2])
+    output = {"::".join(pr_dict.keys()): triple_overlap}
+
+    return output
+
+
+def single_overlap(pr_dict: dict) -> dict:
+    """
+    Calculate the intervals that do not overlap any of the two other pyranges intervals.
+
+    Parameters
+    ----------
+    pr_dict : dict
+      A dict of pyranges intervals.
+    output : dict
+      The intervals that overlap with any of the two other intervals.
+    """
+    bedsingle_output = dict()
+    for bedpair in combinations(pr_dict.keys(), 2):
+        last_overlap = [bed for bed in pr_dict.keys() if bed not in bedpair][0]
+        single_overlap = pr_dict[last_overlap].overlap(pr_dict[bedpair[0]], invert=True)
+        single_overlap = single_overlap.overlap(pr_dict[bedpair[1]], invert=True)
+        bedsingle_output[last_overlap] = single_overlap
+
+    return bedsingle_output
--- tests/test_overlaps.py
+++ tests/test_overlaps.py
@@ -3,6 +3,7 @@ import sys
 import pytest
 import shutil
 import pyranges as pr
+import pandas as pd
 
 from millefeuille.module import overlaps as ov
 
@@ -13,5 +14,106 @@ def test_load_beds():
     result = ov.load_beds(file_beds)
     assert len(result) == 3, "Expected 3 bed files to be loaded"
     assert all(
-        isinstance(bed, pr.PyRanges) for bed in result
+        isinstance(bed, pr.PyRanges) for bed in result.values()
     ), "Each list element should be a PyRanges object"
+
+
+def test_triple_overlap():
+    file_beds = ["./tests/sample1.bed", "./tests/sample2.bed", "./tests/sample3.bed"]
+    pr_dict = ov.load_beds(file_beds)
+    result = ov.triple_overlap(pr_dict)
+    assert len(result) == 1, "Expected 1 overlap region"
+    assert isinstance(result, dict), "Expected result to be a dictionary"
+    assert all(
+        isinstance(bed, pr.PyRanges) for bed in result.values()
+    ), "Each list element should be a PyRanges object"
+    assert list(result.keys()) == ["a::b::c"], "Expected key to be 'a::b::c'"
+    expect_overlap = pd.DataFrame(
+        {
+            "Chromosome": {0: "chr1", 1: "chr1"},
+            "Start": {0: 30, 1: 70},
+            "End": {0: 35, 1: 80},
+            "Name": {0: "peak_1", 1: "peak_2"},
+            "Score": {0: 1000, 1: 1000},
+            "Strand": {0: "+", 1: "+"},
+        }
+    )
+    assert all(
+        result["a::b::c"].as_df() == expect_overlap
+    ), "Expected overlap regions coordinates to match"
+
+
+def test_double_overlap():
+    file_beds = ["./tests/sample1.bed", "./tests/sample2.bed", "./tests/sample3.bed"]
+    pr_dict = ov.load_beds(file_beds)
+    result = ov.double_overlap(pr_dict)
+    assert len(result) == 3, "Expected 3 overlap region pairs"
+    assert isinstance(result, dict), "Expected result to be a dictionary"
+    assert all(
+        isinstance(bed, pr.PyRanges) for bed in result.values()
+    ), "Each list element should be a PyRanges object"
+    assert list(result.keys()) == [
+        "a::b",
+        "a::c",
+        "b::c",
+    ], "Expected key to be 'a::b' 'a::c', 'b::c'"
+    expect_overlap_ab = pd.DataFrame(
+        {
+            "Chromosome": {0: "chr1"},
+            "Start": {0: 100},
+            "End": {0: 110},
+            "Name": {0: "peak_2"},
+            "Score": {0: 1000},
+            "Strand": {0: "+"},
+        }
+    )
+    assert all(
+        result["a::b"].as_df() == expect_overlap_ab
+    ), "Expected overlap regions coordinates to match between a and b"
+    expect_overlap_bc = pd.DataFrame(
+        {
+            "Chromosome": {0: "chr1"},
+            "Start": {0: 150},
+            "End": {0: 160},
+            "Name": {0: "peak_4"},
+        }
+    )
+    assert all(
+        result["b::c"].as_df() == expect_overlap_bc
+    ), "Expected overlap regions coordinates to match between b and c"
+    assert (
+        len(result["a::c"]) == 0
+    ), "Expected no overlap regions coordinates between a and c"
+
+
+def test_single_overlap():
+    file_beds = ["./tests/sample1.bed", "./tests/sample2.bed", "./tests/sample3.bed"]
+    pr_dict = ov.load_beds(file_beds)
+    result = ov.single_overlap(pr_dict)
+    assert len(result) == 3, "Expected 3 overlap regions"
+    assert isinstance(result, dict), "Expected result to be a dictionary"
+    assert all(
+        isinstance(bed, pr.PyRanges) for bed in result.values()
+    ), "Each list element should be a PyRanges object"
+    assert set(result.keys()) == set(
+        [
+            "a",
+            "b",
+            "c",
+        ]
+    ), "Expected key to be 'a', 'b', 'c'"
+    expect_overlap_a = pd.DataFrame(
+        {
+            "Chromosome": {0: "chr1"},
+            "Start": {0: 170},
+            "End": {0: 180},
+            "Name": {0: "peak_3"},
+            "Score": {0: 1000},
+            "Strand": {0: "+"},
+        }
+    )
+    assert all(
+        result["a"].as_df() == expect_overlap_a
+    ), "Expected overlap regions coordinates to match for a"
+    assert len(result["b"]) == 0, "Expected no overlap regions coordinates for b"
+    assert len(result["c"]) == 0, "Expected no overlap regions coordinates for c"
